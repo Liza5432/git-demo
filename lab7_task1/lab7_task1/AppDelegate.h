bdafe4d8578ebd3306cb8219b382437b5ceae462
анус <anus@MBP-anus.bbrouter>
2026-04-28 17:41:07 +0300

    add task1.1

---
+++ lab7_task1/lab7_task1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  lab7_task1
+//
+//  Created by анус on 4/26/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ lab7_task1/lab7_task1/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  lab7_task1
+//
+//  Created by анус on 4/26/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
